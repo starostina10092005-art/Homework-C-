60691552b8342aacbfc6c35652045977498a6820
Dasha <starostina10092005@gmail.com>
2026-07-07 03:07:20 +0300

    Домашее задание

---
+++ домашние задания/lastNumber.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    unsigned int number = 0;
+
+    std::cout << "Введите число: ";
+    std::cin >> number;
+
+    number = number % 10;
+    std::cout << "Последняя цифра: " << number << std::endl;
+
+    return 0;
+}
+
---
+++ домашние задания/sizeof.cpp
@@ -0,0 +1,22 @@
+﻿
+#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+
+    std::cout << "=================================\t\n";
+    std::cout << "|Тип данных\t|Кол-во байт\t|" << std::endl;
+    std::cout << "|bool       \t|" << sizeof(bool) << "\t\t|" << std::endl;
+    std::cout << "|char       \t|" << sizeof(char) << "\t\t|" << std::endl;
+    std::cout << "|short       \t|" << sizeof(short) << "\t\t|" << std::endl;
+    std::cout << "|int       \t|" << sizeof(int) << "\t\t|" << std::endl;
+    std::cout << "|unsigned int \t|" << sizeof(unsigned int) << "\t\t|" << std::endl;
+    std::cout << "|long       \t|" << sizeof(long) << "\t\t|" << std::endl;
+    std::cout << "|long long   \t|" << sizeof(long long) << "\t\t|" << std::endl;
+    std::cout << "|float       \t|" << sizeof(float) << "\t\t|" << std::endl;
+    std::cout << "|double       \t|" << sizeof(double) << "\t\t|" << std::endl;
+    std::cout << "=================================\t\n";
+ 
+}
+
