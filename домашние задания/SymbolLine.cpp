9fbbff94752a5b891ad704fe32d62cb3ea501902
Dasha <starostina10092005@gmail.com>
2026-07-15 14:59:20 +0300

    Линии из символов

---
+++ домашние задания/SymbolLine.cpp
@@ -0,0 +1,43 @@
+﻿
+
+#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+
+    int symbolCount = 0;
+    int i = 0;
+    char symbol = '=';
+    char direction = '-';
+
+    std::cout << "Введите кол-во символов: ";
+    std::cin >> symbolCount;
+    if (symbolCount <= 0) {
+        std::cout << "Неверный выбор!";
+        return 0;
+    }
+    std::cout << "Введите символ: ";
+    std::cin >> symbol;
+
+    std::cout << "Введите направление линии ('-' - горизонтальная, '|' - вертикальная): ";
+    std::cin >> direction;
+
+    if (direction == '-') {
+        while (i < symbolCount) {
+            std::cout << symbol;
+            i++;
+        }
+    }
+    else if (direction == '|') {
+        while (i < symbolCount) {
+            std::cout << symbol << std::endl;
+            i++;
+        }
+    }
+    else {
+        std::cout << "Неизвестное направление! Используйте '-' или '|'." << std::endl;
+        return 0;
+    }
+    return 0;
+}
