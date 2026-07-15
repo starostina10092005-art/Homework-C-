38b43ea5a55d8f8d23615f2cdf1dcba6ea6269e1
Dasha <starostina10092005@gmail.com>
2026-07-16 01:17:29 +0300

    То что вы делали пока я отсутсвовала(Конвертер длины)

---
+++ тренировка/LengthConverter.cpp
@@ -0,0 +1,46 @@
+﻿
+#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+
+    double sm = 0.0;
+    double inch = 0.0;
+    int choise = 1;
+    double const inchSm = 2.54;
+
+    do {
+        std::cout << "Выберите действие:\n1.Перевести дюймы в см\n2.Перевести см в дюймы\n3.Выход\nВаш выбор: ";
+        std::cin >> choise;
+        if (choise <= 0 || choise > 3) {
+            std::cout << "Неверный выбор! Попробуйте снова.\n"; continue;
+        }
+
+        switch (choise) {
+        case 1:
+            std::cout << "Введите дюймы: ";
+            std::cin >> inch;
+            if (inch <= 0) {
+                std::cout << "Неверный выбор\n"; continue;
+            }
+            sm = inch * inchSm;
+            std::cout << "" << inch << " дюйм(а) это " << sm << " сантиметров" << std::endl;
+            break;
+        case 2:
+            std::cout << "Введите сантиметры: ";
+            std::cin >> sm;
+            if (sm <= 0) {
+                std::cout << "Неверный выбор\n"; continue;
+            }
+            inch = sm / inchSm;
+            std::cout << "" << sm << " сантиметр(а) это " << inch << " дюйм(а)" << std::endl;
+            break;
+        case 3:
+            std::cout << "Пока-пока!" << std::endl;
+            break;
+        }
+    } while (choise != 3);
+
+    return 0;
+}
