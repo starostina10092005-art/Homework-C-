958b7b93a8be71300588c2023c9d6361a9221928
Dasha <starostina10092005@gmail.com>
2026-07-20 17:18:18 +0300

    улитка

---
+++ домашние задания/snail.cpp
@@ -0,0 +1,30 @@
+﻿
+#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+
+    int day = 0;
+    int total_distance = 0;
+    int distance = 15;
+
+    std::cout << "Введите кол-во дней: ";
+    std::cin >> day;
+    if (day <= 0) {
+        std::cout << "Неверное значение!" << std::endl;
+        return 0;
+    }
+
+    for (int i = 1; i <= day; i++) {
+        
+        total_distance += distance;
+        distance += 2;
+    
+    }
+
+    std::cout << "Улитка проползла " << total_distance << " см за "
+        << day << " дней" << std::endl;
+
+    return 0;
+}
