4cd4601777e86382849d799bcbe682bca7490066
Dasha <starostina10092005@gmail.com>
2026-07-20 16:29:54 +0300

    задание с кругом 1 вариант

---
+++ домашние задания/circle.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <cmath>                                    
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+
+    double x0{ 0.0 }, y0{ 0.0 };                    
+    double x{ 1.0 }, y{ 1.0 };                      
+
+    double r1{ 1.0 }, r2{ 2.0 };                    
+    double len{ 0.0 };                              
+
+    do {
+        std::cout << "Введите координаты точки (для выхода напишите: 0 0): ";
+        std::cin >> x >> y;
+       
+        if (x == 0 && y == 0) {
+            std::cout << "Пока-пока" << std::endl;
+            break;
+        }
+
+        len = sqrt(pow(x - x0, 2) + pow(y - y0, 2));
+        if (r1 < len && len < r2)
+            std::cout << "Точка на кольце\n";
+        else
+            std::cout << "Точка не на кольце\n";
+
+        
+    } while (true);
+
+    return 0;
+}
